cfd7f7f8896ff02589fc2dd6a7a4859e3bc1af62
Vu Doan <victordoan1708@gmail.com>
2021-01-22 03:28:58 +0000

    ALMOST REPLICATED BASIC LOOK OF THE SETTINGS SECTION TO THE PROTOTYPE, NEED TO FIXED POSITION BUTTONS

--- frontend/src/components/Common/SettingsBoard.vue
+++ frontend/src/components/Common/SettingsBoard.vue
@@ -19,8 +19,64 @@
     </ul>
 
     <hr>
+
     <!-- Personalisation Settings -->
-    <div></div>
+    <div id="settings_section">
+
+      <!-- Checkboxes -->
+      <div class="checkboxes">
+        <div class="titles">
+          <h3>Personalisation</h3>
+        </div>
+        <div class="check_fields">
+          <input id="personal" type="checkbox">
+          <span class="checkmark"></span>
+        </div>
+
+        <div class="titles">
+          <h4>Allow Spotify to Recommend</h4>
+        </div>
+        <div class="check_fields">
+          <input id="spotify" type="checkbox">
+          <span class="checkmark"></span>
+        </div>
+      </div>
+
+      <!-- Inputs Fields -->
+      <div id="input_fields">
+        <p><em>Anything else you would like to manually add in?</em></p>
+
+        <!-- Artists -->
+        <div class="fields" id="artists">
+          <label>Artists</label><br />
+          <input type="text" placeholder="Artist full names">
+        </div>
+
+        <!-- Themes -->
+        <div class="fields" id="themes">
+          <label>Themes</label><br />
+          <input type="text" placeholder="Love, Family,...">
+        </div>
+
+        <!-- Genres -->
+        <div class="fields" id="genres">
+          <label>Genres</label><br />
+          <input type="text" placeholder="Rap, Pop,...">
+        </div>
+
+        <!-- Countries -->
+        <div class="fields" id="countries">
+          <label>Countries</label><br />
+          <input type="text" placeholder="Country's full names">
+        </div>
+      </div>
+    </div>
+
+    <!-- Buttons -->
+    <div ref="btnPosition" class="btnStyling">
+      <div class="decidingBtns" id="save" @click="saveSettings">Save</div>
+      <div class="decidingBtns" id="reset" @click="resetSettings">Reset</div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +94,8 @@ export default {
     const boardRef = ref(null);
     const userDetail = ref({});
 
+    const btnPosition = ref(null);
+
     emitter.on('toggle_settings', (data) => {
       boardRef.value.style.display = 'block';
 
@@ -51,10 +109,22 @@ export default {
       boardRef.value.style.display = 'none';
     }
 
+    function saveSettings() {
+      // console.log('SAVED');
+      // console.log(btnPosition);
+    }
+
+    function resetSettings() {
+      // console.log('RESET');
+    }
+
     return {
       boardRef,
       userDetail,
       closeSettings,
+      btnPosition,
+      saveSettings,
+      resetSettings,
     };
   },
 };
