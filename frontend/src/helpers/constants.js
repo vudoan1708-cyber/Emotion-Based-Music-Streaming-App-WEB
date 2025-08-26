f502c76d300fa53367d62b00d2eb7d2bbe7ba058
Vu Doan <vu@voly.co.uk>
2025-08-26 18:38:51 +0100

    update live url to vercel

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const PRODUCTION = process.env.NODE_ENV;
 export const BUILT_APP_URL = (PRODUCTION === 'production')
-  ? 'https://muserfly.glitch.me'
+  ? 'https://muserfly.vercel.app'
   : 'http://localhost:5000';
-export const DEPLOY_APP_URL = 'https://muserfly.glitch.me';
+export const DEPLOY_APP_URL = 'https://muserfly.vercel.app';
